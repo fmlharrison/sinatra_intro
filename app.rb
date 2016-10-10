0b245c80299f9d2943a3533d1f26dd4920dc3245
Anna Beddow <anna.beddow@gmail.com>
2016-10-10 15:24:00 +0100

    added shotgun

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require "shotgun"
 
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  "beebopbeebopbeeebeebeebeep"
+end
+
+get '/handsome' do
+  "Well, aren't you handsome! ;)"
+end
+
+get '/test' do
+  'and again'
 end
